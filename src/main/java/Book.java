12ca63df6e5a1c9c8790eb85a9f0a005c964000a
per3grin0 <per3grin0.y@gmail.com>
2026-09-14 22:28:24 -0300

    feat: add Book constructor

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -7,10 +7,23 @@ public class Book {
     private String title;
     private String isbn;
     private LinkedHashSet<String> authors;
-    private int pages;
+    private Integer pages;
     private PartialDate pubDate; //Custom type to be created later
     private String description;
-    private int rating;
+    private Integer rating;
     private HashSet<String> tags;
     private ArrayList<Note> notes; //Custom type to be created later
+
+    public Book(String id, String title){
+        this.id = id;
+        this.title = title;
+        this.isbn = null;
+        this.authors = new LinkedHashSet<>();
+        this.pages = null;
+        this.pubDate = null;
+        this.description = null;
+        this.rating = null;
+        this.tags = new HashSet<>();
+        this.notes = new ArrayList<>();
+    }
 }
