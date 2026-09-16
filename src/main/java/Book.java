2a9369feed8e256dee3892f4451501a679244198
per3grin0 <per3grin0.y@gmail.com>
2026-09-16 18:43:56 -0300

    style: enforce braces and use 2-space indentation

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -3,133 +3,151 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 
 public class Book {
-    private String id;
-    private String title;
-    private String isbn;
-    private LinkedHashSet<String> authors;
-    private Integer pages;
-    private PartialDate pubDate; //Custom type to be created later
-    private String description;
-    private Integer rating;
-    private HashSet<String> tags;
-    private ArrayList<Note> notes; //Custom type to be created later
+  private String id;
+  private String title;
+  private String isbn;
+  private LinkedHashSet<String> authors;
+  private Integer pages;
+  private PartialDate pubDate; // Custom type to be created later
+  private String description;
+  private Integer rating;
+  private HashSet<String> tags;
+  private ArrayList<Note> notes; // Custom type to be created later
 
-    public Book(String id, String title) {
-        if (isEmptyOrNull(id)) throw new IllegalArgumentException("ID must not be null");
-        if (isEmptyOrNull(title)) throw new IllegalArgumentException("Title must not be null");
-        this.id = id;
-        this.title = title;
-        this.isbn = null;
-        this.authors = new LinkedHashSet<>();
-        this.pages = null;
-        this.pubDate = null;
-        this.description = null;
-        this.rating = null;
-        this.tags = new HashSet<>();
-        this.notes = new ArrayList<>();
+  public Book(String id, String title) {
+    if (isEmptyOrNull(id)) {
+      throw new IllegalArgumentException("ID must not be null");
     }
+    if (isEmptyOrNull(title)) {
+      throw new IllegalArgumentException("Title must not be null");
+    }
+    this.id = id;
+    this.title = title;
+    this.isbn = null;
+    this.authors = new LinkedHashSet<>();
+    this.pages = null;
+    this.pubDate = null;
+    this.description = null;
+    this.rating = null;
+    this.tags = new HashSet<>();
+    this.notes = new ArrayList<>();
+  }
 
-    public String getId() {
-        return this.id;
-    }
+  public String getId() {
+    return this.id;
+  }
 
-    public String getTitle() {
-        return this.title;
-    }
+  public String getTitle() {
+    return this.title;
+  }
 
-    public void setTitle(String title) {
-        if (isEmptyOrNull(title)) throw new IllegalArgumentException("Title must not be null");
-        this.title = title;
+  public void setTitle(String title) {
+    if (isEmptyOrNull(title)) {
+      throw new IllegalArgumentException("Title must not be null");
     }
+    this.title = title;
+  }
 
-    public Integer getRating() {
-        return this.rating;
-    }
+  public Integer getRating() {
+    return this.rating;
+  }
 
-    public void setRating(Integer rating) {
-        if (rating == null) {
-            this.rating = null;
-        } else if (rating >= 1 && rating <= 5) {
-            this.rating = rating;
-        } else {
-            throw new IllegalArgumentException("Rating must be null or between 1 and 5");
-        }
+  public void setRating(Integer rating) {
+    if (rating == null) {
+      this.rating = null;
+    } else if (rating >= 1 && rating <= 5) {
+      this.rating = rating;
+    } else {
+      throw new IllegalArgumentException("Rating must be null or between 1 and 5");
     }
+  }
 
-    public Integer getPages() {
-        return this.pages;
-    }
+  public Integer getPages() {
+    return this.pages;
+  }
 
-    public void setPages(Integer pages) {
-        if (pages == null) {
-            this.pages = null;
-        } else if (pages > 0) {
-            this.pages = pages;
-        } else {
-            throw new IllegalArgumentException("Pages must be null or greater than 0");
-        }
+  public void setPages(Integer pages) {
+    if (pages == null) {
+      this.pages = null;
+    } else if (pages > 0) {
+      this.pages = pages;
+    } else {
+      throw new IllegalArgumentException("Pages must be null or greater than 0");
     }
+  }
 
-    public String getIsbn() {
-        return this.isbn;
-    }
+  public String getIsbn() {
+    return this.isbn;
+  }
 
-    public void setIsbn(String isbn) {
-        if (isbn == null) {
-            this.isbn = null;
-        } else {
-            String normalIsbn = isbn.replace("-", "");
-            if (normalIsbn.length() == 10 || normalIsbn.length() == 13) {
-                this.isbn = normalIsbn;
-            } else {
-                throw new IllegalArgumentException("ISBN must be 10 or 13-characters long");
-            }
-        }
+  public void setIsbn(String isbn) {
+    if (isbn == null) {
+      this.isbn = null;
+    } else {
+      String normalIsbn = isbn.replace("-", "");
+      if (normalIsbn.length() == 10 || normalIsbn.length() == 13) {
+        this.isbn = normalIsbn;
+      } else {
+        throw new IllegalArgumentException("ISBN must be 10 or 13-characters long");
+      }
     }
+  }
 
-    public String getDescription() {
-        return this.description;
-    }
+  public String getDescription() {
+    return this.description;
+  }
 
-    public void setDescription(String description) {
-        if (description == null) {
-            this.description = null;
-        } else if (description.isEmpty()) {
-            throw new IllegalArgumentException("Description can be null, but not empty");
-        } else {
-            this.description = description;
-        }
+  public void setDescription(String description) {
+    if (description == null) {
+      this.description = null;
+    } else if (description.isEmpty()) {
+      throw new IllegalArgumentException("Description can be null, but not empty");
+    } else {
+      this.description = description;
     }
+  }
 
-    public LinkedHashSet<String> getAuthors(){
-        LinkedHashSet<String> authorSet = new LinkedHashSet<>(this.authors);
-        return authorSet;
-    }
+  public LinkedHashSet<String> getAuthors() {
+    LinkedHashSet<String> authorSet = new LinkedHashSet<>(this.authors);
+    return authorSet;
+  }
 
-    public void addAuthor(String author){
-        if (isEmptyOrNull(author)) throw new IllegalArgumentException("Author must not be empty or null");
-        if (!authors.add(author)) throw new IllegalArgumentException("Author was already added");
+  public void addAuthor(String author) {
+    if (isEmptyOrNull(author)) {
+      throw new IllegalArgumentException("Author must not be empty or null");
     }
+    if (!authors.add(author)) {
+      throw new IllegalArgumentException("Author was already added");
+    }
+  }
 
-    public void removeAuthor(String author){
-        if (!authors.remove(author)) throw new IllegalArgumentException("Author does not exist");
+  public void removeAuthor(String author) {
+    if (!authors.remove(author)) {
+      throw new IllegalArgumentException("Author does not exist");
     }
+  }
 
-    public HashSet<String >getTags(){
-        HashSet<String> tagSet = new HashSet<>(this.tags);
-        return tagSet;
-    }
+  public HashSet<String> getTags() {
+    HashSet<String> tagSet = new HashSet<>(this.tags);
+    return tagSet;
+  }
 
-    public void addTag(String tag){
-        if (isEmptyOrNull(tag)) throw new IllegalArgumentException("Tag must not be empty or null");
-        if (!tags.add(tag)) throw new IllegalArgumentException("Tag was already added");
+  public void addTag(String tag) {
+    if (isEmptyOrNull(tag)) {
+      throw new IllegalArgumentException("Tag must not be empty or null");
     }
+    if (!tags.add(tag)) {
+      throw new IllegalArgumentException("Tag was already added");
+    }
+  }
 
-    public void removeTag(String tag){
-        if (!tags.remove(tag)) throw new IllegalArgumentException("Tag does not exist");
+  public void removeTag(String tag) {
+    if (!tags.remove(tag)) {
+      throw new IllegalArgumentException("Tag does not exist");
     }
+  }
 
-    private boolean isEmptyOrNull(String input) {
-        return input == null || input.isEmpty();
-    }
+  private boolean isEmptyOrNull(String input) {
+    return input == null || input.isEmpty();
+  }
 }
--- src/main/java/PartialDate.java
+++ src/main/java/PartialDate.java
@@ -2,89 +2,96 @@ import java.time.Year;
 import java.util.Comparator;
 
 public class PartialDate implements Comparable<PartialDate> {
-    private static final Comparator<Integer> byMonth = Comparator.nullsFirst(Comparator.<Integer>naturalOrder());
-    private static final Comparator<Integer> byDay = Comparator.nullsFirst(Comparator.<Integer>naturalOrder());
-    private int year;
-    private Integer month;
-    private Integer day;
+  private static final Comparator<Integer> byMonth = Comparator.nullsFirst(Comparator.naturalOrder());
+  private static final Comparator<Integer> byDay = Comparator.nullsFirst(Comparator.naturalOrder());
+  private int year;
+  private Integer month;
+  private Integer day;
 
-    public PartialDate(int year, Integer month, Integer day) {
-        checkYearBoundary(year);
-        checkMonthBoundary(month);
-        checkDayBoundary(day);
-        checkDayHasMonth(month, day);
-        this.year = year;
-        this.month = month;
-        this.day = day;
+  public PartialDate(int year, Integer month, Integer day) {
+    checkYearBoundary(year);
+    checkMonthBoundary(month);
+    checkDayBoundary(day);
+    checkDayHasMonth(month, day);
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  public PartialDate(int year, Integer month) {
+    this(year, month, null);
+  }
+
+  public PartialDate(int year) {
+    this(year, null, null);
+  }
+
+  public Integer getYear() {
+    return this.year;
+  }
+
+  public void setYear(int year) {
+    checkYearBoundary(year);
+    this.year = year;
+  }
+
+  public Integer getMonth() {
+    return this.month;
+  }
+
+  public void setMonth(Integer month) {
+    checkMonthBoundary(month);
+    checkDayHasMonth(month, this.day);
+    this.month = month;
+  }
+
+  public Integer getDay() {
+    return this.day;
+  }
+
+  public void setDay(Integer day) {
+    checkDayBoundary(day);
+    checkDayHasMonth(this.month, day);
+    this.day = day;
+  }
+
+  private void checkYearBoundary(int year) {
+    int currentYear = Year.now().getValue();
+    if (year > currentYear || year < 0) {
+      throw new IllegalArgumentException("Constraint: 0 < year < currentYear");
+    }
+  }
+
+  private void checkMonthBoundary(Integer month) {
+    if (month != null && (month < 1 || month > 12)) {
+      throw new IllegalArgumentException("Constraint: 1 < month < 12");
+    }
+  }
+
+  private void checkDayBoundary(Integer day) {
+    if (day != null && (day < 1 || day > 31)) {
+      throw new IllegalArgumentException("Constraint: 1 < day < 31");
+    }
+  }
+
+  private void checkDayHasMonth(Integer month, Integer day) {
+    if (month == null && day != null) {
+      throw new IllegalArgumentException("Cannot have a day without a month");
+    }
+  }
+
+  @Override
+  public int compareTo(PartialDate other) {
+    int yearDiff = this.year - other.getYear();
+    if (yearDiff != 0) {
+      return yearDiff;
     }
 
-
-    public PartialDate(int year, Integer month) {
-        this(year, month, null);
+    int monthDiff = byMonth.compare(this.month, other.getMonth());
+    if (monthDiff != 0) {
+      return monthDiff;
     }
 
-    public PartialDate(int year) {
-        this(year, null, null);
-    }
-
-    public Integer getYear() {
-        return this.year;
-    }
-
-    public void setYear(int year) {
-        checkYearBoundary(year);
-        this.year = year;
-    }
-
-    public Integer getMonth() {
-        return this.month;
-    }
-
-    public void setMonth(Integer month) {
-        checkMonthBoundary(month);
-        checkDayHasMonth(month, this.day);
-        this.month = month;
-    }
-
-    public Integer getDay() {
-        return this.day;
-    }
-
-    public void setDay(Integer day) {
-        checkDayBoundary(day);
-        checkDayHasMonth(this.month, day);
-        this.day = day;
-    }
-
-    private void checkYearBoundary(int year) {
-        int currentYear = Year.now().getValue();
-        if (year > currentYear || year < 0)
-            throw new IllegalArgumentException("Constraint: 0 < year < currentYear");
-    }
-
-    private void checkMonthBoundary(Integer month) {
-        if (month != null && (month < 1 || month > 12))
-            throw new IllegalArgumentException("Constraint: 1 < month < 12");
-    }
-
-    private void checkDayBoundary(Integer day) {
-        if (day != null && (day < 1 || day > 31))
-            throw new IllegalArgumentException("Constraint: 1 < day < 31");
-    }
-
-    private void checkDayHasMonth(Integer month, Integer day) {
-        if (month == null && day != null)
-            throw new IllegalArgumentException("Cannot have a day without a month");
-    }
-
-    @Override
-    public int compareTo(PartialDate other) {
-        int yearDiff = this.year - other.getYear();
-        if (yearDiff != 0) return yearDiff;
-
-        int monthDiff = byMonth.compare(this.month, other.getMonth());
-        if (monthDiff != 0) return monthDiff;
-
-        return byDay.compare(this.day, other.getDay());
-    }
+    return byDay.compare(this.day, other.getDay());
+  }
 }
