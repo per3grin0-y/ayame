e9d6cc554c8e7801e8ba74cce86c2c281f218336
per3grin0 <per3grin0.y@gmail.com>
2026-09-17 10:19:14 -0300

    refactor: reorder Book fields and methods

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -3,16 +3,16 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 
 public class Book {
-  private String id;
-  private String title;
+  private final String id;
   private String isbn;
+  private String title;
   private LinkedHashSet<String> authors;
-  private Integer pages;
-  private PartialDate pubDate; // Custom type to be created later
   private String description;
   private Integer rating;
+  private Integer pages;
+  private PartialDate pubDate;
   private HashSet<String> tags;
-  private ArrayList<Note> notes; // Custom type to be created later
+  private ArrayList<Note> notes;
 
   public Book(String id, String title) {
     if (isEmptyOrNull(id)) {
@@ -22,58 +22,19 @@ public class Book {
       throw new IllegalArgumentException("Title must not be null");
     }
     this.id = id;
-    this.title = title;
     this.isbn = null;
+    this.title = title;
     this.authors = new LinkedHashSet<>();
-    this.pages = null;
-    this.pubDate = null;
     this.description = null;
     this.rating = null;
+    this.pages = null;
+    this.pubDate = null;
     this.tags = new HashSet<>();
     this.notes = new ArrayList<>();
   }
 
   public String getId() {
     return this.id;
-  }
-
-  public String getTitle() {
-    return this.title;
-  }
-
-  public void setTitle(String title) {
-    if (isEmptyOrNull(title)) {
-      throw new IllegalArgumentException("Title must not be null");
-    }
-    this.title = title;
-  }
-
-  public Integer getRating() {
-    return this.rating;
-  }
-
-  public void setRating(Integer rating) {
-    if (rating == null) {
-      this.rating = null;
-    } else if (rating >= 1 && rating <= 5) {
-      this.rating = rating;
-    } else {
-      throw new IllegalArgumentException("Rating must be null or between 1 and 5");
-    }
-  }
-
-  public Integer getPages() {
-    return this.pages;
-  }
-
-  public void setPages(Integer pages) {
-    if (pages == null) {
-      this.pages = null;
-    } else if (pages > 0) {
-      this.pages = pages;
-    } else {
-      throw new IllegalArgumentException("Pages must be null or greater than 0");
-    }
   }
 
   public String getIsbn() {
@@ -93,18 +54,15 @@ public class Book {
     }
   }
 
-  public String getDescription() {
-    return this.description;
+  public String getTitle() {
+    return this.title;
   }
 
-  public void setDescription(String description) {
-    if (description == null) {
-      this.description = null;
-    } else if (description.isEmpty()) {
-      throw new IllegalArgumentException("Description can be null, but not empty");
-    } else {
-      this.description = description;
+  public void setTitle(String title) {
+    if (isEmptyOrNull(title)) {
+      throw new IllegalArgumentException("Title must not be null");
     }
+    this.title = title;
   }
 
   public LinkedHashSet<String> getAuthors() {
@@ -127,6 +85,57 @@ public class Book {
     }
   }
 
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    if (description == null) {
+      this.description = null;
+    } else if (description.isEmpty()) {
+      throw new IllegalArgumentException("Description can be null, but not empty");
+    } else {
+      this.description = description;
+    }
+  }
+
+
+  public Integer getRating() {
+    return this.rating;
+  }
+
+  public void setRating(Integer rating) {
+    if (rating == null) {
+      this.rating = null;
+    } else if (rating >= 1 && rating <= 5) {
+      this.rating = rating;
+    } else {
+      throw new IllegalArgumentException("Rating must be null or between 1 and 5");
+    }
+  }
+
+  public PartialDate getPubDate() {
+    return this.pubDate;
+  }
+
+  public void setPubDate(PartialDate date) {
+    this.pubDate = date;
+  }
+
+  public Integer getPages() {
+    return this.pages;
+  }
+
+  public void setPages(Integer pages) {
+    if (pages == null) {
+      this.pages = null;
+    } else if (pages > 0) {
+      this.pages = pages;
+    } else {
+      throw new IllegalArgumentException("Pages must be null or greater than 0");
+    }
+  }
+
   public HashSet<String> getTags() {
     HashSet<String> tagSet = new HashSet<>(this.tags);
     return tagSet;
@@ -145,14 +154,6 @@ public class Book {
     if (!tags.remove(tag)) {
       throw new IllegalArgumentException("Tag does not exist");
     }
-  }
-
-  public PartialDate getPubDate() {
-    return this.pubDate;
-  }
-
-  public void setPubDate(PartialDate date) {
-    this.pubDate = date;
   }
 
   public ArrayList<Note> getNotes() {
